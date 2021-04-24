f1718a6cd932c86dc406b94d5e368049abefa32a
jan <janek.zapal@gmail.com>
2021-04-24 13:15:22 +0200

    Dev

--- ui/src/views/Scan.vue
+++ ui/src/views/Scan.vue
@@ -1,6 +1,9 @@
 <template>
   <file-upload :allow-multiple="false" v-on:fileUploaded="handleFileUploaded" />
-  <button v-if="scanInProgress" disabled="disabled" class="btn btn-lg btn-primary">Pracuję...</button>
+  <button v-if="scanInProgress" disabled="disabled" class="btn btn-lg btn-primary">
+    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+    <span class="sr-only">Pracuję...</span>
+  </button>
   <button v-else :disabled="!scanEnabled" class="btn btn-lg btn-primary" @click="scan">Skanuj</button>
 </template>
 <script>
@@ -27,7 +30,7 @@ export default {
         axios.post(`${process.env.VUE_APP_BACKEND_URL}/scan/`, {path: this.filePath}).then(response => {
             this.scanInProgress = false
             window.open(`${process.env.VUE_APP_BACKEND_URL}/${response.data}`)
-        }).catch(error => {
+        }).catch(() => {
             this.scanInProgress = false
             alert('Wystąpił problem!')
         })
